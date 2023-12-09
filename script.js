21ffd3533ea30dc98a92d2d7f547c41cd0e40838
Doginnet <133009034+Doginnet@users.noreply.github.com>
2023-12-09 21:16:39 +0200

    added ""

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function apiGet() {
-    var cityInput = document.getElementById(inputCity).value;
+    var cityInput = document.getElementById("inputCity").value;
     var url = `api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=metric&appid=553b21fc776f72158bae5ef5ea8c7d55`;
 
     var requestOptions = {
