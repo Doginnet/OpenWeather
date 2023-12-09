7865d1745228c776dc6c539853b60f119ba3d1f3
Doginnet <133009034+Doginnet@users.noreply.github.com>
2023-12-09 16:11:10 +0200

    first-version

---
+++ script.js
@@ -0,0 +1,14 @@
+function apiGet() {
+    var cityInput = document.getElementById(inputCity).value;
+    var url = `api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=metric&appid=553b21fc776f72158bae5ef5ea8c7d55`;
+
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+    };
+
+    fetch(url, requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+}
