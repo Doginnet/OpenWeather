22a9b5d09631bdb2c31026f85a96c9be5787eccd
Doginnet <133009034+Doginnet@users.noreply.github.com>
2023-12-09 21:38:34 +0200

    added working api request

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function apiGet() {
     var cityInput = document.getElementById("inputCity").value;
-    var url = `api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=metric&appid=553b21fc776f72158bae5ef5ea8c7d55`;
+    var url = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=metric&appid=553b21fc776f72158bae5ef5ea8c7d55`;
 
     var requestOptions = {
         method: 'GET',
@@ -8,7 +8,11 @@ function apiGet() {
     };
 
     fetch(url, requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
+        .then(response => response.json())
+        .then(data => {
+            var temp = data.main.temp
+console.log("Temperature now is:",temp)
+        })
+        // .then(result => console.log(result))
         .catch(error => console.log('error', error));
 }
