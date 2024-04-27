2cebcf7211d33097f153f7e3e3b777c6738e8b6b
Doginnet <133009034+Doginnet@users.noreply.github.com>
2024-04-27 21:02:56 +0300

    changed the layout

--- script.js
+++ script.js
@@ -8,21 +8,42 @@ function apiGet() {
     };
     var temperature = ""
     var weather = ""
+    let weatherTomorrow = "";
+    let weatherOtherDay = "";
     fetch(url, requestOptions)
         .then(response => response.json())
         .then(data => {
             var temp = data.main.temp
             var weather = data.weather[0].main
             var icon = data.weather[0].icon
-            temperature = "Temperature in " + cityInput + " now is: " + JSON.stringify(temp, null, 2)
+            temperature = "Temperature in " + capitalizeFistChar(cityInput) + " now is: " + JSON.stringify(temp, null, 2)
             iconURL = `https://openweathermap.org/img/wn/${icon}.png`
             weather = JSON.stringify(weather)
             console.log(temperature, weather)
             document.getElementById("response").innerHTML = `${temperature}`;
+            document.getElementById("response").style.display = "block";
             document.getElementById("weatherState").src = iconURL
             document.getElementById("weatherState").innerHTML = `${weather}`;
             document.getElementById("icon").src = iconURL
         })
-       
+        
         .catch(error => console.log('error', error));
+    }
+    
+    window.onload = function () {  //this function will be called when the page is loaded
+        getDate(); 
+}
+
+function getDate () {
+    var d = new Date();
+    var n = d.toDateString().split(' ');
+    n.pop();
+    let formattedDate = n.join('/');
+    document.getElementById("date").innerHTML = formattedDate;
+}
+
+function capitalizeFistChar(city){
+    city = city.split('');
+    city[0] = city[0].toUpperCase();
+    return city.join('');
 }
